79632c07c05888a1641508be6f5da304c04fed1e
NayCS <nali.pei.smile@gmail.com>
2019-10-03 18:41:01 -0400

    post info about purchase

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -12,30 +12,59 @@ module.exports = function (app) {
       });
     });
   });
-  app.post("/person/new", function(req, res) {
-    db.Person.create(req.body).then(function(result) {
-      console.log(result);
-      res.redirect("hat");
-    });
-  });
+  app.post("/person/new", function (req, res) {
+    db.Person.create(req.body).then(function (result) {
+      db.hat.findOne({ where: { hat: req.body.team } }).then(function (result) {
+        res.render("hat", {
+          hat: result
+        });
 
-  // load second "thank you" page and display hat
-  app.get("/hat/:id", function (req, res) {
-    db.hat.findOne({ where: { id: req.params.id } }).then(function (hat) {
-      res.render("hat", {
-        hat: hat
+        db.Post.update(
+          req.body,
+          {
+            where: {
+              id: req.body.id
+            }
+          }).then(function (dbPost) {
+            res.json(dbPost);
+          });
+
+        console.log(result);
+        res.redirect("hat");
       });
     });
-  });
 
 
-  /*
-  deleted code:  
-   res.render("hat", {});
-  */
+    /*
+   
+   
+       db.Post.update(
+         req.body,
+         {
+           where: {
+             id: req.body.id
+           }
+         }).then(function(dbPost) {
+         res.json(dbPost);
+       });
+   
+     // load second "thank you" page and display hat
+     app.get("/hat/:image", function (req, res) {
+       db.hat.findOne({ where: { image: req.params.id } }).then(function (hat) {
+         res.render("hat", {
+           hat: hat
+         });
+       });
+     });
+   
+   
+     
+     deleted code:  
+      res.render("hat", {});
+     */
 
-  // render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
-    res.render("404");
-  });
-};
+    // render 404 page for any unmatched routes
+    app.get("*", function (req, res) {
+      res.render("404");
+    });
+  };
